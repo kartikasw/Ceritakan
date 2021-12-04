4da642e5f409973d26f63db79f99b1c4dc661b3b
kartikasw <kartikasariwidagdo@gmail.com>
2021-12-04 20:13:21 +0700

    fix

--- app/src/main/java/com/example/perempuan/activity/SettingsActivity.kt
+++ app/src/main/java/com/example/perempuan/activity/SettingsActivity.kt
@@ -7,8 +7,6 @@ import android.widget.Button
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.perempuan.R
-import com.example.perempuan.databinding.ActivitySettingsBinding
-import io.reactivex.Observable
 
 class SettingsActivity : AppCompatActivity() {
 
